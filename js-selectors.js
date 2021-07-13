169ae746365c59f02a9b04525c029a3fc79d5d23
Raza Ul Kareem <83599698+rez4president@users.noreply.github.com>
2021-07-13 22:17:01 +0530

    Add files via upload

---
+++ js-selectors.js
@@ -0,0 +1,17 @@
+// let name = document.getElementsByName("wish")
+// console.dir(name)
+
+// let tag = document.getElementsByTagName("p")
+// console.dir(tag)
+
+// let className = document.getElementsByClassName("red-text")
+// console.dir(className);
+
+// let id = document.getElementById("main");
+// console.dir(id)
+
+// let query = document.querySelector(".red-text");
+// console.dir(query);
+
+let querySelector = document.querySelectorAll(".red-text");
+console.dir(querySelector);
